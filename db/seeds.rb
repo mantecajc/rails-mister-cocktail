41d942ecbf7de8c19a58f8777af4cf0f448609d5
Javier Cuadrado <mantecajc@gmail.com>
2021-02-11 16:00:35 +0100

    Added dose nesting route, controller & simple form in show.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
+# require 'faker'
+
+# puts "Cleaning data base"
+# Cocktail.destroy_all
+# Dose.destroy_all
+# Ingredient.destroy_all
+
+# puts "Creating ingredients"
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+# puts "Creating cocktails..."
+
+# 20.times do
+#   Cocktail.create(name: Faker::Coffee.blend_name)
+# end
+
+# puts "Finished!"
+require 'json'
+require 'open-uri'
 
 puts "Cleaning data base"
 Cocktail.destroy_all
 Dose.destroy_all
 Ingredient.destroy_all
 
-puts "Creating ingredients"
-
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-
 puts "Creating cocktails..."
 
 20.times do
   Cocktail.create(name: Faker::Coffee.blend_name)
+end
+
+puts "Creating ingredients..."
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+
+user["drinks"].each do |item|
+  Ingredient.create(name: item["strIngredient1"])
 end
 
 puts "Finished!"
