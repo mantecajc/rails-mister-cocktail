86a60fd24f1b065a095a29d8d9252d0d837d721f
Javier Cuadrado <mantecajc@gmail.com>
2021-02-12 15:08:23 +0100

    Added cl_image_tag & styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'cocktails/index'
+  root to: 'cocktails#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails do
     resources :doses, only: [:create]
